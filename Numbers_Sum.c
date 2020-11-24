b776f14203661375cac2d2c1069afae9b901d7b0
U-h-s-e-R <reshmakrishna04@gmail.com>
2020-11-24 06:35:20 +0530

    third commit

---
+++ Numbers_Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nPROGRAM TO ADD TWO NUMBERS");
+    printf("\nEnter the first number: ");
+    scanf("%d",&a);
+    printf("\nEnter the second number: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum of %d and %d is %d",a,b,c);
+    return 0;
+}
