07edfd039a4a3f93439bebb260ccf96a847a77fa
U-h-s-e-R <reshmakrishna04@gmail.com>
2020-11-24 09:36:06 +0530

    fifth commit

---
+++ Square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a, result;
+    Printf("Enter the integer number: ");
+    scanf("%d",&a);
+    result=a*a;
+    printf("\nSquare of %d = %d",a,result);
+    return 0;
+}
